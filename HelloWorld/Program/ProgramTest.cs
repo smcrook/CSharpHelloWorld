382ebdeb0435b0fad712c946126c7f423e123643
Scott Crook <scott.crook@macom.com>
2017-06-01 09:00:38 -0400

    Adding Simple NUnit tests

---
+++ HelloWorld/Program/ProgramTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    [TestFixture]
+    public class ProgramTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+
+        [Test]
+        public void FailMethod()
+        {
+            Assert.Fail("We Expect to fail");
+        }
+    }
+}
